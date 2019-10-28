86203924cb38d74dae56b56f7ef63c8868a22394
ASUS <Alviandwipramono12@gmail.com>
2019-10-28 10:21:03 +0700

    first commit

---
+++ python.py
@@ -0,0 +1,23 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=input("masukan nilai a:")
+masukan nilai a:20
+>>> b=input("masukan nilai b:")
+masukan nilai b:2
+>>> print(hasil penggabungan {1}&{0}=%d" .format(20,2) %(20+2))
+      
+SyntaxError: invalid syntax
+>>> print("hasil penggabungan {1}&{0}=%d" .format(20,2) %(20+2))
+hasil penggabungan 2&20=22
+>>> #Konversi nilai variable
+>>> a=int(20)
+>>> b=int(2)
+>>> print("hasil penjumlahan {1}+{0}=%d" .format(20,2) %(20+2))
+hasil penjumlahan 2+20=22
+>>> print("hasil pembagian {1}/{0}=%d" .format(20,2) %(20/2))
+hasil pembagian 2/20=10
+>>> hasil penggabungan 2&20=22
+SyntaxError: invalid syntax
+>>> print
+<built-in function print>
+>>> 
